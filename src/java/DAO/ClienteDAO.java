9353ed876af0154562dbf62642aa7c908232aaa2
hz <hz@192.168.0.10>
2019-02-01 00:23:31 -0200

    First commit - Conexão com o banco, resgatando dados. Bancos separados, ainda não sei fazer relacional.

--- src/java/DAO/ClienteDAO.java
+++ src/java/DAO/ClienteDAO.java
@@ -27,7 +27,7 @@ public class ClienteDAO {
     }
     public ArrayList<Cliente> readAll(){
         try{
-            String sql = "SELECT * FROM Tbl_Cliente";//comando para ir na tabela do BD
+            String sql = "SELECT * FROM Cliente";//comando para ir na tabela do BD
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql); // traduza esse comando para um do sql
             //result set, conjunto, é a representacao do select
             ResultSet rs = ps.executeQuery(); // sempre aponta para uma posicao de antes dos resultados
@@ -35,8 +35,10 @@ public class ClienteDAO {
             ArrayList<Cliente> lista = new ArrayList<Cliente>();
             while(rs.next()){
                 Cliente cli = new Cliente();
-                cli.setIdade(rs.getInt("Idade")); //nome do atributo na tabela.
-                cli.setNome(rs.getString("Nome"));
+                cli.setNome(rs.getString("nome")); //nome do atributo na tabela.
+                cli.setIdade(rs.getInt("idade"));
+                cli.setEndereco(rs.getString("endereco"));
+                cli.setN_conta(rs.getInt("id_conta"));
                 lista.add(cli);
                 //fazer isto para todos os atributos
             }
@@ -51,7 +53,10 @@ public class ClienteDAO {
         return null;
     }
     
-    public boolean insertAll(Cliente c){
+    /*public boolean insertAll(Cliente c){
+        //mandar para o Banco.16:27:26	delete from dbzivi.Tbl_Cliente where Nome = 'Denise'	Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column.  To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.	0.00080 sec
+
+        
         try{
             String sql = "INSERT * INTO Tbl_Cliente";//comando para ir na tabela do BD
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql); // traduza esse comando para um do sql
@@ -76,5 +81,5 @@ public class ClienteDAO {
             System.err.println("Erro geral " + ex.getMessage());
         }
         return false;
-    }
+    } */
 }

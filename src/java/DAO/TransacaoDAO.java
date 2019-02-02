4f06969275b12646350400930dca0376251ab1d7
hz <hz@192.168.0.10>
2019-02-02 13:50:31 -0200

    Insere, atualiza, remove e busca - todos menos Transacao.

--- src/java/DAO/TransacaoDAO.java
+++ src/java/DAO/TransacaoDAO.java
@@ -16,116 +16,138 @@ import java.util.ArrayList;
 public class TransacaoDAO {
 
     private DataSource dataSource;
-    public TransacaoDAO(DataSource dataSource){
+
+    public TransacaoDAO(DataSource dataSource) {
         //
-        this.dataSource = dataSource; 
-        
+        this.dataSource = dataSource;
+
     }
-     
-    public boolean insertAll_Transacao(ArrayList<Transacao> vetTransacao){
+
+    public boolean insertAll_Transacao(ArrayList<Transacao> vetTransacao) {
         //mandar para o Banco.16:27:26	delete from dbzivi.Tbl_Cliente where Nome = 'Denise'	Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column.  To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.	0.00080 sec
 
-      
-        try{
+        try {
             //inserindo
-            String sql = "INSERT INTO dbzivi.Transacao (id_conta, data_hora, valor) values (?,?,?)";
+            String sql = "INSERT INTO dbzivi.Transacao (idTransacao, id_conta, valor) values (?,?,?)";
 
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql); // traduza esse comando para um do sql
-            
-            for(Transacao c: vetTransacao){
+
+            for (Transacao c : vetTransacao) {
                 //posicoes na tabela do BD
-                ps.setInt(1, c.getId_conta());
-                ps.setString(2, c.getData_hora());
+                ps.setString(1, c.getIdTransacao());
+                ps.setInt(2, c.getId_conta());
                 ps.setFloat(3, c.getValor());
-
                 ps.executeUpdate();
-            }  
+            }
             ps.close();
             return true;
-                    
-        }catch(SQLDataException ex){
-            System.err.println("[Conta] - Erro ao recuperar dados "+ex.getMessage());
-        }catch(Exception ex){
-            System.err.println("[Conta] - Erro geral " + ex.getMessage());
+
+        } catch (SQLDataException ex) {
+            System.err.println("[Transacao] - Erro ao recuperar dados " + ex.getMessage());
+        } catch (Exception ex) {
+            System.err.println("[Transacao] - Erro geral " + ex.getMessage());
         }
         return false;
     }
-    public boolean insertOne_Transacao(Transacao tr){
-        
-        try{
-            //inserindo
-            String sql = "INSERT INTO dbzivi.Transacao (id_conta, data_hora, valor) values (?,?,?)";
+
+    public boolean insertOne_Transacao(Transacao tr) {
+
+        try {
+            String sql = "INSERT INTO dbzivi.Transacao (idTransacao, id_conta, valor) values (?,?,?)";
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql); // traduza esse comando para um do sql
-                //posicoes na tabela do BD
-                ps.setInt(1, tr.getId_conta());
-                ps.setString(2, tr.getData_hora());
-                ps.setFloat(3, tr.getValor());
-
-                ps.executeUpdate();
             
+            ps.setString(1, tr.getIdTransacao());
+            ps.setInt(2, tr.getId_conta());
+            ps.setFloat(3, tr.getValor());
+            ps.executeUpdate();
+
             ps.close();
             return true;
-                    
-        }catch(SQLDataException ex){
-            System.err.println("[Conta] - Erro ao recuperar dados "+ex.getMessage());
-        }catch(Exception ex){
-            System.err.println("[Conta] - Erro geral " + ex.getMessage());
+
+        } catch (SQLDataException ex) {
+            System.err.println("[Transacao] - Erro ao inserir dados " + ex.getMessage());
+        } catch (Exception ex) {
+            System.err.println("[Transacao] - Erro geral " + ex.getMessage());
         }
         return false;
-        
+
     }
-    public ArrayList<Transacao> readAll_Transacao(){
-        try{
+
+    public ArrayList<Transacao> readAll_Transacao() {
+        try {
             String sql = "SELECT * FROM Transacao";//comando para ir na tabela do BD
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql); // traduza esse comando para um do sql
             //result set, conjunto, é a representacao do select
             ResultSet rs = ps.executeQuery(); // sempre aponta para uma posicao de antes dos resultados
             //se houver um registro ele começa recuperar antes deles
             ArrayList<Transacao> lista = new ArrayList<Transacao>();
-            while(rs.next()){
+            while (rs.next()) {
                 Transacao trans = new Transacao();
+                trans.setIdTransacao(rs.getString("idTransacao"));
                 trans.setId_conta(rs.getInt("id_conta")); //nome do atributo na tabela.
-                trans.setData_hora(rs.getString("data_hora"));
-                trans.setValor(rs.getFloat("valor"));    
+                trans.setValor(rs.getFloat("valor"));
                 lista.add(trans);
                 //fazer isto para todos os atributos
             }
             ps.close();
             return lista;
-                    
-        }catch(SQLDataException ex){
-            System.err.println("[Transacao] - Erro ao recuperar dados "+ex.getMessage());
-        }catch(Exception ex){
+
+        } catch (SQLDataException ex) {
+            System.err.println("[Transacao] - Erro ao recuperar dados " + ex.getMessage());
+        } catch (Exception ex) {
             System.err.println("[Transacao] - Erro geral " + ex.getMessage());
         }
         return null;
     }
-    public ArrayList<Transacao> readOne_Transacoes(int numDaConta){
-        try{
-            String sql = "SELECT * FROM Transacao where id_conta="+numDaConta;//comando para ir na tabela do BD
+
+    public ArrayList<Transacao> readOne_Transacoes(int numDaConta) {
+        try {
+            String sql = "SELECT * FROM Transacao where id_conta=" + numDaConta;//comando para ir na tabela do BD
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql); // traduza esse comando para um do sql
             //result set, conjunto, é a representacao do select
             ResultSet rs = ps.executeQuery(); // sempre aponta para uma posicao de antes dos resultados
             //se houver um registro ele começa recuperar antes deles
             ArrayList<Transacao> lista = new ArrayList<Transacao>();
-            while(rs.next()){
+            while (rs.next()) {
                 Transacao trans = new Transacao();
+                trans.setIdTransacao(rs.getString("idTransacao"));
                 trans.setId_conta(rs.getInt("id_conta")); //nome do atributo na tabela.
-                trans.setData_hora(rs.getString("data_hora"));
-                trans.setValor(rs.getFloat("valor"));    
+                trans.setValor(rs.getFloat("valor"));
                 lista.add(trans);
                 //fazer isto para todos os atributos
             }
             ps.close();
             return lista;
-                    
-        }catch(SQLDataException ex){
-            System.err.println("[Transacao] - Erro ao recuperar dados "+ex.getMessage());
-        }catch(Exception ex){
+
+        } catch (SQLDataException ex) {
+            System.err.println("[Transacao] - Erro ao recuperar dados " + ex.getMessage());
+        } catch (Exception ex) {
             System.err.println("[Transacao] - Erro geral " + ex.getMessage());
         }
         return null;
-    }   
-    
-  
+    }
+
+    public boolean delete_Transacao(Transacao t) {
+
+        
+            try {
+                String sql = "DELETE FROM Transacao WHERE idTransacao = ?";
+                PreparedStatement ps = dataSource.getConnection().prepareStatement(sql);
+                ps.setString(1, t.getIdTransacao());
+                
+                ps.executeUpdate();
+                ps.close();
+                return true;
+
+            } catch (SQLDataException ex) {
+                System.err.println("[Transacao] - Erro ao excluir dados " + ex.getMessage());
+            } catch (Exception ex) {
+                System.err.println("[Transacao] - Erro geral " + ex.getMessage());
+            }
+        
+        return false;
+    }
+
+
+
 }
--- src/java/Model/Transacao.java
+++ src/java/Model/Transacao.java
@@ -10,19 +10,26 @@ package Model;
  * @author hz
  */
 public class Transacao {
+    private String idTransacao;
     private int id_conta;
-    private String data_hora;
     private float valor;
 
     public Transacao() {
+        this.idTransacao = "";
         this.id_conta = 0;
-        this.data_hora = "";
         this.valor = 0.0f;
     }
-    public Transacao(int id_conta, String data_hora, float valor) {
+    public Transacao(String idTransacao, int id_conta, float valor) {
+        this.idTransacao = idTransacao;
         this.id_conta = id_conta;
-        this.data_hora = data_hora;
         this.valor = valor;
+    }
+    
+    public String getIdTransacao() {
+        return idTransacao;
+    }
+    public void setIdTransacao(String idTransacao) {
+        this.idTransacao = idTransacao;
     }
     public int getId_conta() {
         return id_conta;
@@ -30,22 +37,14 @@ public class Transacao {
     public void setId_conta(int id_conta) {
         this.id_conta = id_conta;
     }
-    public String getData_hora() {
-        return data_hora;
-    }
-    public void setData_hora(String data_hora) {
-        this.data_hora = data_hora;
-    }
     public float getValor() {
         return valor;
     }
     public void setValor(float valor) {
         this.valor = valor;
     }
-    
     public void printTransacao(){
-        System.out.printf("%s %s %d %s %f \n", "Data: "+this.getData_hora()," Origem:", this.getId_conta()," Valor:", this.getValor());
+        System.out.printf("%s %s %d %s %f \n", "Data/Hora/Origem "+this.getIdTransacao()," Conta: ", this.getId_conta()," Valor:", this.getValor());
     }
-    
     
 }
